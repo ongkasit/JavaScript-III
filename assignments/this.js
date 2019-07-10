9f0a0069169ec4d6a807a9ecc0f0738597ec5182
Kasit Nganthavee <kasit.nganthavee@gmail.com>
2019-07-10 08:17:31 +0700

    completed this keyword exercise

--- assignments/this.js
+++ assignments/this.js
@@ -7,23 +7,88 @@
  * notation to call the object that is the left to the dot. 
  * 3. New binding: is when you create a completely new tree within that same forest analogy. The New binding refers to constructor
  * function, which is not yet covered thus far in the course.
- * 4. Explicit binding: is when you use .call .apply .binding 
+ * 4. Explicit binding: is when you use .call .apply .binding to work with arrays
  *
  * write out a code example of each explanation above
  */
 
 // Principle 1
-
 // code example for Window Binding
+function sayName(name) {
+    console.log(this);
+    return name;
+}
+
+console.log(sayName("Kasit Nganthavee"));
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+        console.log(`${this.greeting} my name is ${name}`);
+        console.log(this);
+    }
+};
+myObj.sayHello('Ong');
 
+const sayNameFunc = obj => {
+    obj.sayName = function() {
+        console.log(`Hello my name is ${this.name}`);
+        console.log(this);
+    };
+};
+
+const me = { name: 'Tim' };
+const you = { name: 'Leon' };
+sayNameFunc(me);
+sayNameFunc(you);
+
+//Invoke Methods on objects
+me.sayName();
+you.sayName();
+
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+        console.log(this.greeting + this.greeter);
+        console.log(this);
+    };
+}
 
+const bright = new CordialPerson('Alex');
+const alex = new CordialPerson('Bright');
+
+bright.speak();
+alex.speak();
+
+// Principle 4
 // code example for Explicit Binding
+
+bright.speak.call(alex);
+alex.speak.call(bright);
+
+let ultimateName = function() {
+    console.log(`This is the ultimate name: ${this.name}`);
+};
+
+let kasit = {
+    name: 'Kasit',
+    age: 22
+};
+
+let languages = ['Javascript', 'Python', 'Ruby'];
+
+ultimateName.call(kasit);
+
+let ultimateSkill = function(lang1, lang2, lang3) {
+    console.log(`My name is ${this.name} and I know ${lang1} ${lang2} and ${lang3}`);
+};
+
+ultimateSkill.apply(kasit, languages);
